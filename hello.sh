4b7cb8cb2523f6918f147053540c1994191330b7
Aaron Wright <aaron.c.wright@icloud.com>
2024-03-09 11:40:38 -0600

    Add makefile

--- hello.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Build
-cl65 -t atari2600 -o hello.bin -l hello.list hello.asm
-
-# Run
-stella hello.bin
---
+++ makefile
@@ -0,0 +1,5 @@
+make:
+	cl65 -t atari2600 -o hello.bin -l hello.list hello.asm
+
+run:
+	stella hello.bin
